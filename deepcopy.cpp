c9019fadf7dce40823ce66899c23043c8adaed95
apurvakumbhar <apurvakumbhar097@gmail.com>
2025-03-21 11:29:51 +0530

    Add files via upload

---
+++ deepcopy.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Demo{
+	private:
+		int *p;
+	public:
+		void getInfo(){
+			cout<<*p<<endl;
+		}	
+		void setInfo(int v){
+			*this->p=v;
+		}
+		Demo(int x){
+			p=new int(x);
+		}
+		Demo(const Demo & obj){
+			p=obj.p;
+			p=new int(*(obj.p));//deep copy
+	    }
+		~Demo(){
+			cout<<"Destructor is called"<<endl;
+	}
+		
+};
+int main()
+{
+	Demo d1(111);
+	d1.getInfo();
+	Demo d2(d1);
+	d2.getInfo();
+	d2.setInfo(222);
+	d2.getInfo();
+	d1.getInfo();
+	
+	return 0;
+}
